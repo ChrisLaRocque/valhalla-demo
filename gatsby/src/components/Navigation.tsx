57d0862ed3cbfd59dd8f109426a9a7b7a01bda1c
ChrisLaRocque <larocque.christopher@gmail.com>
2023-07-21 13:06:12 -0400

    Adds headless ui

--- gatsby/src/components/Navigation.tsx
+++ gatsby/src/components/Navigation.tsx
@@ -1,20 +1,116 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "gatsby";
+import { Menu, Transition } from "@headlessui/react";
+
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
 export default function Navigation({}) {
   return (
-    <nav className="mx-auto my-9 flex max-w-7xl justify-between">
+    <nav className="mx-auto my-9 flex max-w-7xl items-center justify-between">
       <Link className="text-xl font-bold" to="/">
         Money &amp; Money Capital
       </Link>
-      <ul id="nav-items" className="lg:flex">
+      {/* <ul id="nav-items" className="lg:flex">
         <li className="lg:pl-4">
           <Link to="/companies/">Companies</Link>
         </li>
         <li className="lg:pl-4">
           <Link to="/blog/">Blog</Link>
         </li>
-      </ul>
+      </ul> */}
+      <HeadlessMenu />
     </nav>
+  );
+}
+function HeadlessMenu() {
+  return (
+    <div className="text-right">
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black px-4 py-2 text-sm font-medium text-white hover:bg-opacity-80 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+            Menu
+            <ChevronDownIcon
+              className="-mr-1 ml-2 h-5 w-5 text-violet-200 hover:text-violet-100"
+              aria-hidden="true"
+            />
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="px-1 py-1 ">
+              <Menu.Item>
+                {({ active }) => (
+                  <Link
+                    to="/companies"
+                    className={`${
+                      active ? "bg-violet-500 text-white" : "text-gray-900"
+                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                  >
+                    Companies
+                  </Link>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <Link
+                    to="/blog"
+                    className={`${
+                      active ? "bg-violet-500 text-white" : "text-gray-900"
+                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                  >
+                    Blog
+                  </Link>
+                )}
+              </Menu.Item>
+            </div>
+            {/* <div className="px-1 py-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={`${
+                      active ? "bg-violet-500 text-white" : "text-gray-900"
+                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                  >
+                    Archive
+                  </button>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={`${
+                      active ? "bg-violet-500 text-white" : "text-gray-900"
+                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                  >
+                    Move
+                  </button>
+                )}
+              </Menu.Item>
+            </div>
+            <div className="px-1 py-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={`${
+                      active ? "bg-violet-500 text-white" : "text-gray-900"
+                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                  >
+                    Delete
+                  </button>
+                )}
+              </Menu.Item>
+            </div> */}
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
   );
 }
